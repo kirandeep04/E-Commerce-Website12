755a1fd78d16ebe4e7c2630f6d9a39fbc10453eb
kirandeepkaurfzr <kirandeepkaurfzr4@gmail.com>
2023-12-01 11:22:23 +0530

    first_Commit

---
+++ E-Commerce Website/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Website.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLogInController : Controller
+    {
+      
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult Confirm() 
+        {
+            return View(); 
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Commerce Website/Data/CustomContext.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce_Website.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Website.Data
+{
+    public class CustomContext:DbContext
+    {
+        public CustomContext(DbContextOptions<CustomContext> option) : base(option) { }
+        public DbSet<AdminLogin> AdminLogins { get; set; }
+    }
+}
